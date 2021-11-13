c97c99ae7c9b7b75da24f4ae781e5799250b45f6
bbz11 <shadowtroll42@gmail.com>
2021-11-13 01:43:27 +0100

    menu calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { CalendarDate, CaretUp } from 'react-bootstrap-icons'
+import { calendarItems } from './constants/'
 
 function Calendar() {
-  return <div className='Calendar'>Calendar</div>
+  return(
+  <div className='Calendar'>
+    <div className="header">
+      <div className="title">
+        <CalendarDate size= "20" />
+        <p>Calendar</p>
+      </div>
+      <div className="buttons">
+      <span>
+        <CaretUp size="24" />
+      </span>
+      </div>
+    </div>
+    <div className="items">
+      {
+      calendarItems.map(item =>
+        <div className="item" key={item}>
+          {item}
+        </div>
+        )}
+    </div>
+  </div>
+  )
 }
 
 export default Calendar
---
+++ src/components/constants/index.js
@@ -0,0 +1,2 @@
+//da mozemo koristit gdje god ocemo
+export const calendarItems = ["Today","Next 7 days","All days"]
